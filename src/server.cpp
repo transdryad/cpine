81b8eaf9cbba43d728d5d0c374d067f9efb3cc15
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-11-18 18:39:03 -0700

    can read all in input packet except unsigned short for some reason

--- src/server.cpp
+++ src/server.cpp
@@ -8,12 +8,54 @@
 #include <netdb.h>
 #include <cstdio>
 #include <unistd.h>
+#include <array>
+#include <valarray>
+#include <cstring>
+#include <vector>
+#include <cstdint>
 
 #include "src/server.hpp"
+
+#define SEGMENT_BITS 0x7f
+#define CONTINUE_BIT 0x80
 
 Server::Server(std::string ip, std::string port) {
     this->ip = ip;
     this->port = port;
+}
+
+int readVarInt(std::vector<char>& bytes, bool eraseFlag) {
+    int value = 0;
+    int position = 0;
+    int byteCounter = 0;
+    char currentByte;
+    while (true) {
+        currentByte = bytes[byteCounter];
+        value |= (currentByte & SEGMENT_BITS) << position;
+        position += 7;
+        ++byteCounter;
+        if (!(currentByte & CONTINUE_BIT)) break;
+    }
+    //std::cout << "Position: " << position << std::endl;
+    if (eraseFlag) bytes.erase(bytes.begin(), bytes.begin() + byteCounter);
+    return value;
+}
+
+std::string readString(std::vector<char>& bytes, bool eraseFlag) {
+    int length = readVarInt(bytes, true);
+    std::string output = "";
+    for (int i = 0; i < length; i++) {
+        output += bytes[i];
+    }
+    if (eraseFlag) bytes.erase(bytes.begin(), bytes.begin() + length);
+    return output;
+}
+
+int readUShort(std::vector<char>& bytes, bool eraseFlag) {
+    unsigned short native =  ((unsigned short)bytes[0] << 8) | bytes[1];
+    //native = ntohs(native);
+    if (eraseFlag) bytes.erase(bytes.begin(), bytes.begin() + 2);
+    return (int)native;
 }
 
 int Server::run() {
@@ -54,6 +96,44 @@ int Server::run() {
     client_sock = accept(sock, (struct sockaddr *)&client_addr, &addr_size);
 
     std::cout << "Client connected!" << std::endl;
+    
+    int max_size = 1024;
+    char buffer[max_size];
+    std::vector<char> bytes;
+    std::vector<char> packet;
+    int packlength;
+
+    while (true) { //get full packet into bytes.
+        recv(client_sock, &buffer, max_size, 0);
+        for (int i = 0; i < max_size; i++) {
+            bytes.push_back(buffer[i]);
+            buffer[i] = 0;
+        }
+        packlength = readVarInt(bytes, false);
+        if (bytes.size() >= packlength + 1) { break; }
+    }
+    readVarInt(bytes, true);
+
+    std::copy(bytes.begin(), bytes.begin() + packlength, std::back_inserter(packet));
+    bytes.erase(bytes.begin(), bytes.begin() + packlength);
+
+    std::cout << "Raw Packet:";
+    for (char c : packet) {
+        std::cout << ", 0x";
+        printf("%x", c);
+    }
+    std::cout << std::endl;
+
+    //std::cout << "Packet Length: " << readVarInt(packet, true) << std::endl;
+    int packID;
+    packID = readVarInt(packet, true);
+    //std::cout << packID << std::endl;
+    switch (packID) {
+        case 0:
+            std::cout << "Protocol Version: " << readVarInt(packet, true) << std::endl;
+            std::cout << "Hostname: " << readString(packet, true) << std::endl;
+            std::cout << "Port: " << readUShort(packet, true) << std::endl;
+    }
 
     close(client_sock);
     close(sock);
