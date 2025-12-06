06bffe67333420927ed79aa31533006de0e9f4fc
Hazel Viswanath <viswanath.hazel@gmail.com>
2025-12-05 22:47:11 -0700

    rework networking to make any kind of sense

--- src/server.cpp
+++ src/server.cpp
@@ -13,11 +13,14 @@
 #include <cstring>
 #include <vector>
 #include <cstdint>
+#include <fcntl.h>
 
 #include "src/server.hpp"
+#include "src/client.hpp"
 
 #define SEGMENT_BITS 0x7f
 #define CONTINUE_BIT 0x80
+//#define MAX_PLAYERS 20
 
 Server::Server(std::string ip, std::string port) {
     this->ip = ip;
@@ -39,6 +42,19 @@ int readVarInt(std::vector<char>& bytes, bool eraseFlag) {
     //std::cout << "Position: " << position << std::endl;
     if (eraseFlag) bytes.erase(bytes.begin(), bytes.begin() + byteCounter);
     return value;
+}
+
+void writeVarInt(std::vector<char>& bytes, int value) {
+    //int i = 0;
+    //unsigned int uval = (unsigned int)value;
+    while (true) {
+        if ((value & ~SEGMENT_BITS) == 0) {
+            bytes.push_back(value);
+            return;
+        }
+        bytes.push_back((value & SEGMENT_BITS) | CONTINUE_BIT);
+        value = value >> 7;
+    }
 }
 
 std::string readString(std::vector<char>& bytes, bool eraseFlag) {
@@ -78,7 +94,9 @@ int Server::run() {
         perror("Error getting socket"); return 1;
     }
 
-    std::cout << " on socket:" <<sock << std::endl;
+    std::cout << " on socket:" << sock << std::endl;
+
+    fcntl(sock, F_SETFL, O_NONBLOCK);
 
     int yes = 1;
     setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
@@ -90,6 +108,31 @@ int Server::run() {
         perror("Error listening on port"); return 1;
     }
 
+    std::cout << "Listening on port " << port << std::endl;
+    
+    struct addrinfo *client_addr;
+    socklen_t addr_size = sizeof(client_addr);
+    
+    int client = 0;
+
+    while (true) { //better way, multiple clients
+        clients.emplace_back(accept(sock, (struct sockaddr *)&client_addr, &addr_size), NONE); //try new player
+        if (clients.back().sockfd == -1) { //failed accept or no client
+            clients.pop_back();
+        } else {
+            fcntl(clients.back().sockfd, F_SETFL, O_NONBLOCK);
+            std::cout << "New client: " << clients.back().sockfd << std::endl;
+        }
+        //TODO: tick
+        int cfd = clients[client].sockfd;
+        
+        
+    }
+
+    for (Client c : clients) {
+        close(c.sockfd);
+    }
+    /* old method - kept for notes on packet handling
     int client_sock;
     struct sockaddr_storage client_addr;
     socklen_t addr_size = sizeof(client_addr);
@@ -103,41 +146,56 @@ int Server::run() {
     std::vector<char> packet;
     int packlength;
 
-    while (true) { //get full packet into bytes.
-        recv(client_sock, &buffer, max_size, 0);
-        for (int i = 0; i < max_size; i++) {
-            bytes.push_back(buffer[i]);
-            buffer[i] = 0;
+    while (true) {
+        while (true) { //get full packet into bytes.
+            recv(client_sock, &buffer, max_size, 0);
+            for (int i = 0; i < max_size; i++) {
+                bytes.push_back(buffer[i]);
+                buffer[i] = 0;
+            }
+            packlength = readVarInt(bytes, false);
+            if (bytes.size() >= packlength + 1) { break; }
         }
-        packlength = readVarInt(bytes, false);
-        if (bytes.size() >= packlength + 1) { break; }
+        readVarInt(bytes, true);
+
+        std::copy(bytes.begin(), bytes.begin() + packlength, std::back_inserter(packet));
+        bytes.erase(bytes.begin(), bytes.begin() + packlength);
+
+        std::cout << "Raw Packet: ";
+        for (char c : packet) {
+            std::cout << "0x";
+            printf("%x", c);
+            std::cout << ", ";
+        }
+        std::cout << std::endl;
+
+        //std::cout << "Packet Length: " << readVarInt(packet, true) << std::endl;
+        int packID;
+        packID = readVarInt(packet, true);
+        //std::cout << packID << std::endl;
+        switch (packID) {
+            case 0:
+                std::cout << "Protocol Version: " << readVarInt(packet, true) << std::endl;
+                std::cout << "Hostname: " << readString(packet, true) << std::endl;
+                std::cout << "Port: " << readUShort(packet, true) << std::endl;
+                status = readVarInt(packet, true);
+                std::cout << "Intent (1 - status, 2 - login, 3 - transfer): " << status << std::endl;
+                switch (status) {
+                    case 1:
+                        if (bytes[0] == 0) { //ping & pong
+                            bytes.erase(bytes.begin(), bytes.begin() + 1);
+                            packet.clear();
+                            packet.push_back(0x00);
+                            writeVarInt(packet, strlen(status_str.c_str()));
+
+                        }
+                }
+        }
     }
-    readVarInt(bytes, true);
-
-    std::copy(bytes.begin(), bytes.begin() + packlength, std::back_inserter(packet));
-    bytes.erase(bytes.begin(), bytes.begin() + packlength);
-
-    std::cout << "Raw Packet:";
-    for (char c : packet) {
-        std::cout << ", 0x";
-        printf("%x", c);
-    }
-    std::cout << std::endl;
-
-    //std::cout << "Packet Length: " << readVarInt(packet, true) << std::endl;
-    int packID;
-    packID = readVarInt(packet, true);
-    //std::cout << packID << std::endl;
-    switch (packID) {
-        case 0:
-            std::cout << "Protocol Version: " << readVarInt(packet, true) << std::endl;
-            std::cout << "Hostname: " << readString(packet, true) << std::endl;
-            std::cout << "Port: " << readUShort(packet, true) << std::endl;
-    }
-
     close(client_sock);
+    */
     close(sock);
     freeaddrinfo(servinfo);
-
+    freeaddrinfo(client_addr);
     return 0;
 }
--- src/server.hpp
+++ src/server.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <vector>
+#include "src/client.hpp"
+
 class Server {
     public:
         Server(std::string ip, std::string port);
         std::string ip;
         std::string port;
         int run();
+        int state = 0;
+        std::string status_str = "{\"version\": {\"name\": \"1.21.8\",\"protocol\": 772},\"players\": {\"max\": 20,\"online\": 0,},\"description\": {\"text\": \"Testing!\"]},\"enforcesSecureChat\": false}";
+        std::vector<Client> clients;
 };
